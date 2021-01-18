17893fd7618a3b5c7191f03f49b02ecb329cf7d5
Maxim-nwb <73755542+Maxim-nwb@users.noreply.github.com>
2021-01-18 15:18:20 +0300

    Game mechanics
    Added:
    - Increasing the snake when eating food
    - Correct creation of a new part of the snake
    - Correct body movement

--- game_entities.py
+++ game_entities.py
@@ -2,16 +2,33 @@ import pygame
 import random
 
 class Snake(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x, y, index):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((10, 10))
         self.image.fill((0, 255, 0))
         self.rect = self.image.get_rect()
-        self.rect.center = (300, 250)
+        self.rect.x = x
+        self.rect.y = y
+        # coordinates of the previous step
+        self.x_last = None
+        self.y_last = None
+        # index in the sprite group
+        self.index = index
         
     def update(self, x, y):
-        self.rect.x += x
-        self.rect.y += y
+        # if the head, then sets the movement
+        if self.index == 0:
+            self.x_last = self.rect.x
+            self.y_last = self.rect.y
+            self.rect.x += x
+            self.rect.y += y
+        # otherwise, follows the top body
+        else:
+            top_body = self.groups()[0].get_sprite(self.index - 1)
+            self.x_last = self.rect.x
+            self.y_last = self.rect.y
+            self.rect.x = top_body.x_last
+            self.rect.y = top_body.y_last
 
 class Food(pygame.sprite.Sprite):
     def __init__(self):
@@ -19,6 +36,15 @@ class Food(pygame.sprite.Sprite):
         self.image = pygame.Surface((10, 10))
         self.image.fill((255, 255, 0))
         self.rect = self.image.get_rect()
+
+    def chek_eating(self, other):
+        # checking that the player has eaten the food
+        x = other.rect.x
+        y = other.rect.y
+        if (self.rect.x in range(x - 10, x + 10, 1)) and (self.rect.y in range(y - 10, y + 10, 1)):
+            return True
+        else:
+            return False
 
     def update(self):
         self.rect.x = random.randint(1, 599)
--- main.py
+++ main.py
@@ -7,57 +7,77 @@ from game_entities import *
 pygame.init()
 surface = pygame.display.set_mode((600, 500))
 clock = pygame.time.Clock()
+
 # init menu
 settings_menu = SettingsMenu()
 game_menu = GameMenu(settings_menu)
+
 # init entities
-all_sprites = pygame.sprite.Group()
+# init snake
+snake = pygame.sprite.LayeredUpdates()
+snake_head = Snake(300, 250, 0)
+snake.add(snake_head)
 food_sprite = pygame.sprite.Group()
-snake = Snake()
+
+# init food
 food = Food()
-all_sprites.add(snake)
 food_sprite.add(food)
 food_sprite.update()
-
 
 # variable for controlling movements
 x_mov = 0       
 y_mov = 0
+last_mov = None # last direction of movement
+
 # variable for loop control
 running = True
+
+# game loop
 while running:
-    clock.tick(1)
-    # game_menu.mainloop(surface)
+    clock.tick(5)
+    #game_menu.mainloop(surface)
     for event in pygame.event.get():
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
         # snake movements
+        # change the direction and remember
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 x_mov = -10
                 y_mov = 0
+                last_mov = "LEFT"
             elif event.key == pygame.K_RIGHT:
                 x_mov = 10
                 y_mov = 0
+                last_mov = "RIGHT"
             elif event.key == pygame.K_UP:
                 x_mov = 0
                 y_mov = -10
+                last_mov = "UP"
             elif event.key == pygame.K_DOWN:
                 x_mov = 0
                 y_mov = 10
+                last_mov = "DOWN"
 
     # stop game if snake touched the borders
-    if snake.rect.x >= 600 or snake.rect.x < 0 or snake.rect.y >= 500 or snake.rect.y < 0:
+    if snake_head.rect.x >= 600 or snake_head.rect.x < 0 or snake_head.rect.y >= 500 or snake_head.rect.y < 0:
         running = False
 
     # update screen
-    all_sprites.update(x_mov, y_mov)
-    if (snake.rect.x == food.rect.x) and (snake.rect.y == food.rect.y):
+    snake.update(x_mov, y_mov)
+    if food.chek_eating(snake_head):
+        # get coordinates of the last element of the snake
+        tail_x = snake.get_sprite(-1).rect.x
+        tail_y = snake.get_sprite(-1).rect.y
+        # adding a new element of the snake
+        snake.add(Snake(tail_x, tail_y, len(snake)))
+        # creating new food
         food_sprite.update()
+    # update playing field
     surface.fill((0, 0, 0))
     food_sprite.draw(surface)
-    all_sprites.draw(surface)
+    snake.draw(surface)
     
     pygame.display.flip()
 
